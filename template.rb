dea676fec804482cbf0103ed2cee1c4d4747ed6e
Phuwanart Larpmark <phuwanart@outlook.com>
2021-10-08 15:24:28 +0700

    run rails template from source_path

--- template.rb
+++ template.rb
@@ -5,21 +5,20 @@ require "shellwords"
 # copy_file and template resolve against our source files. If this file was
 # invoked remotely via HTTP, that means the files are not present locally.
 # In that case, use `git clone` to download them to a local temporary dir.
-@tempdir = Dir.mktmpdir("rails-templates-lite")
 
 def add_template_repository_to_source_path
   if __FILE__ =~ %r{\Ahttps?://}
     require "tmpdir"
-    source_paths.unshift(@tempdir)
-    at_exit { FileUtils.remove_entry(@tempdir) }
+    source_paths.unshift(tempdir = Dir.mktmpdir("rails-templates-lite"))
+    at_exit { FileUtils.remove_entry(tempdir) }
     git clone: [
       "--quiet",
       "https://github.com/phuwanart/rails-templates-lite.git",
-      @tempdir
+      tempdir
     ].map(&:shellescape).join(" ")
 
     if (branch = __FILE__[%r{rails-templates-lite/(.+)/template.rb}, 1])
-      Dir.chdir(@tempdir) { git checkout: branch }
+      Dir.chdir(tempdir) { git checkout: branch }
     end
   else
     source_paths.unshift(File.dirname(__FILE__))
@@ -30,5 +29,6 @@ add_template_repository_to_source_path
 
 after_bundle do
   # apply 'templates/devise/template.rb'
-  rails_command "app:template LOCATION=\"#{@tempdir}/templates/devise/template.rb\""
+  # rails_command "app:template LOCATION=\"#{@tempdir}/templates/devise/template.rb\""
+  rails_command "app:template LOCATION=#{File.expand_path("./templates/devise/template.rb",  __dir__)}"
 end
