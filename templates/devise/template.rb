e22dd1a81bfcf0e67f01f6688c25f31eb65ad37e
Phuwanart Larpmark <phuwanart@outlook.com>
2021-10-08 17:15:13 +0700

    run rails template from source_path

--- templates/devise/template.rb
+++ templates/devise/template.rb
@@ -3,3 +3,10 @@ gem 'devise', github: 'strobilomyces/devise', branch: 'patch-1'
 Bundler.with_unbundled_env { run 'bundle install' }
 
 generate 'devise:install'
+
+say 'Default application.html.erb is missing!', :red
+if defined?(Turbo)
+  say %(        Add <%= stylesheet_link_tag "application", "data-turbo-track": "reload" %> within the <head> tag in your custom layout.)
+else
+  say %(        Add <%= stylesheet_link_tag "application" %> within the <head> tag in your custom layout.)
+end
