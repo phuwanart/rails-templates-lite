43129dbc9268869395f40489952f2b7a39fb854a
Phuwanart Larpmark <phuwanart@outlook.com>
2021-10-08 15:02:15 +0700

    general improvement

--- template.rb
+++ template.rb
@@ -16,7 +16,7 @@ def add_template_repository_to_source_path
       tempdir
     ].map(&:shellescape).join(" ")
 
-    if (branch = __FILE__[%r{rails-template/(.+)/template.rb}, 1])
+    if (branch = __FILE__[%r{rails-templates-lite/(.+)/template.rb}, 1])
       Dir.chdir(tempdir) { git checkout: branch }
     end
   else
